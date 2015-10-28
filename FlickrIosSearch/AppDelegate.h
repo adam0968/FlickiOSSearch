febac2322d0ea753ee5d8cb1d239f2ca247773b9
Adam Hellewell <adam@builtbyhq.com>
2015-10-28 17:29:42 -0600

    Initial Commit

---
+++ FlickrIosSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrIosSearch
+//
+//  Created by Adam Hellewell on 10/28/15.
+//  Copyright © 2015 Adam Hellewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrIosSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrIosSearch
+//
+//  Created by Adam Hellewell on 10/28/15.
+//  Copyright © 2015 Adam Hellewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
